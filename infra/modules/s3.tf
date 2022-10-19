d78cc9b536d31651e2141455c4af407079f3dc00
Richard Cosemans <richardc@Richards-MacBook-Pro-2.local>
2022-10-19 22:32:55 +0200

    Invalid bucket name fix

--- infra/modules/s3.tf
+++ infra/modules/s3.tf
@@ -1,19 +1,19 @@
-resource "aws_s3_bucket" "Random-Tweet" {
-  bucket = "Random-Tweet-Bucket"
+resource "aws_s3_bucket" "random-tweet" {
+  bucket = "random-tweet-bucket"
 
 }
 
-resource "aws_kms_key" "Random-Tweet-Key" {
+resource "aws_kms_key" "random-tweet-key" {
   description             = "This key is used to encrypt bucket objects in the Random-Tweet bucket."
   deletion_window_in_days = 7
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.Random-Tweet.bucket
+  bucket = aws_s3_bucket.random-tweet.bucket
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.Random-Tweet-Key.arn
+      kms_master_key_id = aws_kms_key.random-tweet-key.arn
       sse_algorithm     = "aws:kms"
     }
   }
