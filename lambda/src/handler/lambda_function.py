2debb5fca958c8aef831752f5165ecc95cecaab3
Richard Cosemans <richardc@Richards-MacBook-Pro-2.local>
2022-11-01 11:39:21 +0100

    Updated function and test 1 of makign everything work

--- lambda/src/handler/lambda_function.py
+++ lambda/src/handler/lambda_function.py
@@ -1,9 +1,42 @@
 import unzip_requirements
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import os
 import boto3
+import tweepy
+import random
 
 s3 = boto3.resource("s3")
+
+api_token = os.getenv("API_TOKEN")
+api_secret = os.getenv("API_TOKEN_SECRET")
+bearer_token = os.getenv("BEARER_TOKEN")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+
+def create_tweet(text: str) -> None:
+    client = tweepy.Client(
+        bearer_token=bearer_token,
+        consumer_key=api_token,
+        consumer_secret=api_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
+    )
+    # tweepy.API(client)
+    client.create_tweet(text=text)
+
+
+def generate_random_tweet(model, tokenizer, input: str) -> str:
+    input_ids = tokenizer(input, return_tensors="pt").input_ids
+    outputs = model.generate(
+        input_ids,
+        do_sample=True,
+        max_length=50,
+        top_k=50,
+        top_p=0.95,
+    )
+
+    return tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
 
 
 def download_s3_folder(bucket_name, s3_folder, local_dir=None):
@@ -28,12 +61,40 @@ def download_s3_folder(bucket_name, s3_folder, local_dir=None):
         bucket.download_file(obj.key, target)
 
 
+def get_trend() -> str:
+    # make the API connection
+    auth = tweepy.OAuth2BearerHandler(bearer_token)
+    api = tweepy.API(auth)
+
+    # get list with dictionaries with information of locations available to get trending topics
+    trending_places = api.available_trends()
+
+    # return a random place - get the list of an element using randint
+    random_place = trending_places[random.randint(0, len(trending_places) - 1)]
+
+    # get the trending topics from that place
+    random_woeid = random_place["woeid"]
+    random_trends = api.get_place_trends(random_woeid)[0]["trends"]
+
+    # get a random trend
+    random_trend = random_trends[random.randint(0, len(random_trends) - 1)]["name"]
+
+    return random_trend
+
+
 def lambda_handler(event, context: dict) -> dict:
     download_s3_folder("random-tweet-bucket", "lambda/model/", local_dir="/tmp/model/")
-    model = AutoModelForCausalLM.from_pretrained("/tmp/model")
-    tokenizer = AutoTokenizer.from_pretrained("/tmp/model")
-    prompt = "Today I believe we can finally"
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+    tokenizer = GPT2Tokenizer.from_pretrained("/tmp/model")
+    model = GPT2LMHeadModel.from_pretrained(
+        "/tmp/model", pad_token_id=tokenizer.eos_token_id
+    )
+    trend = get_trend()
+    random_tweet = generate_random_tweet(model, tokenizer, trend)
+    text_to_tweet = f""" 
+    {random_tweet}
+    
+    #{trend}
+    """
+    create_tweet(text_to_tweet)
 
-    outputs = model.generate(input_ids, do_sample=True, max_length=30)
-    return tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    return {"Tweet": "Successfully tweeted."}
--- lambda/unzip_requirements/__init__.py
+++ lambda/unzip_requirements/__init__.py
@@ -3,18 +3,18 @@ import shutil
 import sys
 import zipfile
 
-pkg_dir = '/tmp/torch'
+pkg_dir = "/tmp/torch"
 
 # Add package_dir to our path so Python will know to look there for packages
-sys.path.insert(1, '/tmp')
+sys.path.insert(1, "/tmp")
 
 if not os.path.exists(pkg_dir):
-    temp_dir = '/tmp/_torch'
+    temp_dir = "/tmp/_torch"
     if os.path.exists(temp_dir):
         shutil.rmtree(temp_dir)
 
-    python_package_root = '/opt/python'  # location of installed Python packages
-    zip_requirements = os.path.join(python_package_root, 'torch.zip')
+    python_package_root = "/opt/python"  # location of installed Python packages
+    zip_requirements = os.path.join(python_package_root, "torch.zip")
 
-    zipfile.ZipFile(zip_requirements, 'r').extractall(temp_dir)
+    zipfile.ZipFile(zip_requirements, "r").extractall(temp_dir)
     os.rename(temp_dir, pkg_dir)
