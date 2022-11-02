d98a3477367a0691a0648be18ff6ae2f6f19425d
Richard Cosemans <richardc@Richards-MacBook-Pro-2.local>
2022-11-02 22:59:48 +0100

    Updated cron

--- infra/main.tf
+++ infra/main.tf
@@ -9,5 +9,5 @@ module "base_infra" {
   TWITTER_API_KEY_ID        = var.TWITTER_API_KEY_ID
   TWITTER_API_KEY_SECRET    = var.TWITTER_API_KEY_SECRET
   TWITTER_BEARER_TOKEN      = var.TWITTER_BEARER_TOKEN
-  schedule                  = "cron(0 14 * * * *)"
+  schedule                  = "cron(0 14 * * ? *)"
 }
